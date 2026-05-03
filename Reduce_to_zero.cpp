3f1b087987e46c450e27c2215403247d70db70de
itsAryan1123 <ar.aryan2311@gmail.com>
2026-05-03 12:47:05 +0530

    Create Reduce_to_zero.cpp

---
+++ Reduce_to_zero.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int n = num;
+        int count = 0;
+        
+        while( n > 0 ){
+            if ( n % 2 == 0 ){
+                n = n / 2;
+            } else {
+                n = n - 1;
+            }
+            count = count + 1;
+        }
+        
+        return count; 
+    }
+};
